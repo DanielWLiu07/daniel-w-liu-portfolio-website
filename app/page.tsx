c6efb5ec323662046683774694a1ef71b7fc19a4
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2025-12-23 19:12:19 -0500

    Update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,37 @@ import Scene from '@/components/three/Scene';
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-white">
-      <Scene />
+    <div className="relative w-full h-screen bg-white">
+      <div className="absolute w-full h-full z-10">
+        <div className="flex justify-center items-center">
+         <video
+  className="h-[110px] w-auto"
+  src="/name_videos/first_name.webm"
+  autoPlay
+  muted
+  playsInline
+/>
+<video
+  className="w-auto"
+  src="/name_videos/middle_name.webm"
+  autoPlay
+  muted
+  playsInline
+/>
+<video
+  className="h-[200px] w-auto"
+  src="/name_videos/last_name.webm"
+  autoPlay
+  muted
+  playsInline
+/>
+        </div>
+        
+
+      </div>
+      <div className="z-0 absolute w-full h-full">
+            <Scene />
+      </div>
     </div>
   );
 }
