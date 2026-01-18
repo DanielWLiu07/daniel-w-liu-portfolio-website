b2eefb33262e968fa1139f578de44fd67e93de82
Daniel W Liu <117604131+LTKers@users.noreply.github.com>
2026-01-18 12:22:09 -0500

    fix: make experience header text scale responsively below 461px

--- components/experience/experience-header.tsx
+++ components/experience/experience-header.tsx
@@ -11,7 +11,7 @@ export function ExperienceHeader() {
         alt=""
         width={600}
         height={200}
-        className="w-auto h-auto max-w-[300px] min-[431px]:max-w-[450px] lg:max-w-full absolute top-2 left-1/2 -translate-x-1/2 lg:left-auto lg:translate-x-0 lg:-left-2"
+        className="w-auto h-auto max-w-[80%] min-[461px]:max-w-[450px] lg:max-w-full absolute top-2 left-1/2 -translate-x-1/2 lg:left-auto lg:translate-x-0 lg:-left-2"
         style={{ filter: SHADOW_FILTER }}
         aria-hidden="true"
       />
@@ -20,7 +20,7 @@ export function ExperienceHeader() {
         alt="Experience"
         width={600}
         height={200}
-        className="w-auto h-auto max-w-[300px] min-[431px]:max-w-[450px] lg:max-w-full relative z-20 mx-auto lg:mx-0"
+        className="w-auto h-auto max-w-[80%] min-[461px]:max-w-[450px] lg:max-w-full relative z-20 mx-auto lg:mx-0"
         style={{ filter: GLOW_FILTER }}
         priority
       />
